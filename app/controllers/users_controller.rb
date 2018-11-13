bc810a706e7284c46cf007217ff2662e7b51d648
Johnfelix Espinosa <johnfelix.espinosa@icloud.com>
2018-11-13 12:43:25 -0500

    Users can login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,5 +16,10 @@ class UsersController < ApplicationController
   private
     
     def user_params
+      params.require(:user).permit(:username,
+        :email,
+        :password,
+        :password_confirmation
+        )
     end
 end
