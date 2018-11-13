52e6e34e9d6a0c9be8ff73da21de843fa14fcb55
Johnfelix Espinosa <johnfelix.espinosa@icloud.com>
2018-11-13 17:32:43 -0500

    battlestation validations

--- app/models/battlestation.rb
+++ app/models/battlestation.rb
@@ -1,4 +1,16 @@
 class Battlestation < ApplicationRecord
   belongs_to :user
-  
+
+  validates :name,             presence: true
+  validates :cpu,              presence: true
+  validates :cpu_cooler,       presence: true
+  validates :motherboard,      presence: true
+  validates :storage,          presence: true
+  validates :video_card,       presence: true
+  validates :case,             presence: true
+  validates :power_supply,     presence: true
+  validates :operating_system, presence: true
+  validates :monitor,          presence: true
+  validate :image_validated?
+
 end
