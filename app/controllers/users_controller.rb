ad6692274c44a1133862e1e7107b0d3797422ee9
Johnfelix Espinosa <johnfelix.espinosa@icloud.com>
2018-11-13 13:32:24 -0500

    Clean up Users Controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,27 +1,7 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
 
-  def new
-    @user = User.new
+  def show
   end
 
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      redirect_to user_path(@user)
-    else
-      render 'new'
-    end
-  end
-
-  private
-    
-    def user_params
-      params.require(:user).permit(
-        :username,
-        :email,
-        :password,
-        :password_confirmation
-        )
-    end
 end
