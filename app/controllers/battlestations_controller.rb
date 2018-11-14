fffa50ae146568b3b2f0597c50489bf20637ad68
Johnfelix Espinosa <johnfelix.espinosa@icloud.com>
2018-11-14 15:03:09 -0500

    Add edit and delete buttons for index view for battlestations

--- app/controllers/battlestations_controller.rb
+++ app/controllers/battlestations_controller.rb
@@ -1,5 +1,5 @@
 class BattlestationsController < ApplicationController
-  before_action :authenticate_user!, except: [:index]
+  before_action :authenticate_user!
 
   def index
     if params[:user_id]
