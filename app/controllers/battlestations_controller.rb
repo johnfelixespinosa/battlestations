f2453bf4e65ac02892ac717d0d1ce3e919700533
Johnfelix Espinosa <johnfelix.espinosa@icloud.com>
2018-11-13 20:13:20 -0500

    Add Explore option on homepage, viewable by all, not required to sign in

--- app/controllers/battlestations_controller.rb
+++ app/controllers/battlestations_controller.rb
@@ -1,5 +1,5 @@
 class BattlestationsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:index]
 
   def index
     if params[:user_id]
